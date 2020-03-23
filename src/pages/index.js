86bab3c3b98cde4a66b7a6b26bb8066832d3fdad
Jeremy Gottfried <jeremyagottfried@gmail.com>
2020-03-23 15:01:34 -0400

    pages index.js

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import PhotosPage from './PhotosPage';
+import HomePage from './HomePage';
+import MusicPage from './MusicPage';
+import ContactPage from './ContactPage';
+import AboutPage from './AboutPage';
+
+export { PhotosPage, HomePage, MusicPage, ContactPage, AboutPage };
