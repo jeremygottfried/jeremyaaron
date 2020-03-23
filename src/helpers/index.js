8abbfe8519b17e347b63dba6672a0f9aa5a305c1
Jeremy Gottfried <jeremyagottfried@gmail.com>
2020-03-23 15:01:54 -0400

    helpers/utils index.js

---
+++ src/helpers/index.js
@@ -0,0 +1,5 @@
+import snakeToCamel from './snakeToCamel';
+import kebabToCamel from './kebabToCamel';
+import upcaseFirstLetter from './upcaseFirstLetter';
+
+export { snakeToCamel, kebabToCamel, upcaseFirstLetter };
